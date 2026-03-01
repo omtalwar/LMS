3427d56c1dfd650b0c8318e506e2ae93c2394c62
Om Talwar <omtalwar7@gmail.com>
2026-03-01 20:38:07 +0530

    main

--- courses/create_admin.py
+++ courses/create_admin.py
@@ -1,17 +1,15 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lms_project.settings")
-django.setup()
-
 from django.contrib.auth.models import User
+import os
 
 username = os.getenv("DJANGO_SUPERUSER_USERNAME")
 email = os.getenv("DJANGO_SUPERUSER_EMAIL")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superuser created")
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
 else:
-    print("Superuser already exists")
+    print("Missing environment variables")
